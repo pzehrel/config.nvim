c58af84ced54483a9656f58ba13a5b79a3ebc0f7
pzehrel <pzehrel@gmail.com>
2026-06-20 15:18:06 +0800

    refactor(nvim): 删除 Insert 模式 jk 退出映射

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,7 +6,6 @@ local map = vim.keymap.set
 local del = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
